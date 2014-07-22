d05248aa07e48f7073ffcb560b57fce062c76db5
acttoz <acttoz@2f1ed029-a5f6-0410-9981-ff4d102dcfa4>
2014-07-22 03:08:46 +0000

    git-svn-id: https://dev.naver.com/svn/black13@105 2f1ed029-a5f6-0410-9981-ff4d102dcfa4

---
+++ space_balloon2/Assets/scr_quest.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scr_quest : MonoBehaviour
+{
+		public enum questID
+		{
+				SHOT,
+				SPACE,
+				KILL,
+				ALIVE1,
+				ALIVE2,
+				COLLECT
+		}
+		public questID quest;
+		private int level;
+		public int[] conLevel;
+		public int[] conPoint;
+		// Use this for initialization
+		void Start ()
+		{
+				switch (quest) {
+				case questID.SHOT: 
+
+						break;
+				case questID.SPACE:
+						break;
+				case questID.KILL:
+						break;
+				case questID.ALIVE1:
+						break;
+				case questID.ALIVE2:
+						break;
+				case questID.COLLECT:
+						break;
+				default:
+						break;
+				}
+				GetComponentInChildren<tk2dTextMesh> ().text = ";"; 
+					
+
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+}
