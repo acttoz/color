9bb741e3fcb4ff369eabb200f154aba91476b5b5
acttoz <acttoz@2f1ed029-a5f6-0410-9981-ff4d102dcfa4>
2014-09-27 13:15:30 +0000

    git-svn-id: https://dev.naver.com/svn/black13@140 2f1ed029-a5f6-0410-9981-ff4d102dcfa4

--- color/Assets/00000Color/script/color_mat.cs
+++ color/Assets/00000Color/script/color_mat.cs
@@ -3,161 +3,164 @@ using System.Collections;
 
 public class color_mat : MonoBehaviour
 {
-		private bool isPlus = false;
-		private bool isMinus = false;
+	private bool isPlus = false;
+	private bool isMinus = false;
 //		private Transform whiteMAT_position;
 //		private GameObject whiteMat;
 //		private SpriteRenderer whiteMatSprite;
 //		private Animation[] whiteMAT_anim;
 //		private SpriteRenderer matSprite;
-		private EasySprite_HSV saturation;
-		private EasySprite_Pattern pattern;
-		public GameObject effectSuccess;
-		public GameObject oClone;
-		public AudioClip sSuccess;
-		public EasySprite_HSV cHSV;
-		public Texture2D rainbow;
+	private EasySprite_HSV saturation;
+//		private EasySprite_Pattern pattern;
+	public GameObject effectSuccess;
+	public GameObject oClone;
+	public AudioClip sSuccess;
+	public EasySprite_HSV cHSV;
+	public Texture2D rainbow;
 
-		void Start ()
-		{
+	void Start ()
+	{
 //				whiteMat = gameObject.transform.parent.gameObject;
 //				whiteMatSprite = whiteMat.GetComponent<SpriteRenderer> ();
 //				matSprite = GetComponent<SpriteRenderer> ();
 //				pattern = GetComponent<EasySprite_Pattern> ();
-				if (gameObject.name.Equals ("mat"))
-						makeRainbow ();
-		}
-		// Use this for initialization
-		void reset ()
-		{
+		if (gameObject.name.Equals ("mat"))
+			makeRainbow ();
+	}
+	// Use this for initialization
+	void reset ()
+	{
 //				saturation.enabled = false;
 		
 //				if (oClone != null)
 //						Destroy (oClone);
 			
-				CancelInvoke ("minusColor");
-				CancelInvoke ("plusColor");
+		CancelInvoke ("minusColor");
+		CancelInvoke ("plusColor");
 //				whiteMAT_position = GetComponentInChildren<Transform> ();
 //		whiteMAT_position.localPosition = new Vector3 (0, 0, 0);
 //				whiteMAT_anim = GetComponentsInChildren<Animation> ();
 //				whiteMatSprite.sortingOrder = matSprite.sortingOrder - 1;
 //				whiteMatSprite.sortingLayerName = matSprite.sortingLayerName;
 //				GetComponent<SpriteRenderer> ().color *= new Color (1, 1, 1, 0);
-				InvokeRepeating ("plusColor", 0, 0.01f);
-				InvokeRepeating ("minusColor", 0, 0.01f);
-				offColor ();
-		}
+		InvokeRepeating ("plusColor", 0, 0.01f);
+		InvokeRepeating ("minusColor", 0, 0.01f);
+		offColor ();
+	}
 
-		void makeRainbow ()
-		{
-				oClone = Instantiate (this.gameObject, this.transform.position, Quaternion.identity) as GameObject;
-				oClone.tag = "Untagged";
-				pattern = oClone.AddComponent ("EasySprite_Pattern") as EasySprite_Pattern;
-				pattern.__MainTex2 = rainbow;
-				pattern._AutoScrollY = true;
-				pattern._AutoScrollSpeedY = -2.5f;
-				//				pattern.enabled = true;
-				oClone.GetComponent<SpriteRenderer> ().sortingOrder = GetComponent<SpriteRenderer> ().sortingOrder + 1;
-				oClone.GetComponent<PolygonCollider2D> ().enabled = false;
-				oClone.renderer.material.shader = Shader.Find ("EasySprite2D/Pattern_EasyS2D");
-		
-				oClone.SetActive (false);
-				//				saturation.enabled = true;
-				saturation = gameObject.AddComponent ("EasySprite_HSV") as EasySprite_HSV;
-				saturation._HueShift = 0;
-				renderer.material.shader = Shader.Find ("EasySprite2D/HSV_EasyS2D");
-		}
+	void makeRainbow ()
+	{
+//				oClone = Instantiate (this.gameObject, this.transform.position, Quaternion.identity) as GameObject;
+//				oClone.tag = "Untagged";
+//				pattern = oClone.AddComponent ("EasySprite_Pattern") as EasySprite_Pattern;
+//				pattern.__MainTex2 = rainbow;
+//				pattern._AutoScrollY = true;
+//				pattern._AutoScrollSpeedY = -2.5f;
+//				//				pattern.enabled = true;
+//				oClone.GetComponent<SpriteRenderer> ().sortingOrder = GetComponent<SpriteRenderer> ().sortingOrder + 1;
+//				oClone.GetComponent<PolygonCollider2D> ().enabled = false;
+//				oClone.renderer.material.shader = Shader.Find ("EasySprite2D/Pattern_EasyS2D");
+//		
+//				oClone.SetActive (false);
+		//				saturation.enabled = true;
+		saturation = GetComponent ("EasySprite_HSV") as EasySprite_HSV;
+		saturation._HueShift = 0;
+		renderer.material.shader = Shader.Find ("EasySprite2D/HSV_EasyS2D");
+	}
 	
-		// Update is called once per frame
-		void Update ()
-		{
+	// Update is called once per frame
+	void Update ()
+	{
 				 
-		}
+	}
 
-		void plusColor ()
-		{
+	void plusColor ()
+	{
 //				Debug.Log (GetComponent<SpriteRenderer> ().color.a + "");
-				if (!isPlus || !STATE._STATE.Equals ("gIDLE")) {
-						endPlus ();
-						return;
-				}
-				if (saturation._Saturation < 1) {
-						starPlus ();
+		if (!isPlus || !STATE._STATE.Equals ("gIDLE")) {
+			endPlus ();
+			return;
+		}
+		if (saturation._Saturation < 5) {
+			starPlus ();
 
 //						cpnt_whiteMAT_sprite [1].color = new Color (1, 0, 0);
-						pattern._Alpha = 1 - saturation._Saturation;
-						saturation._Saturation += RATE.colorPlusRate / 10000f;
-						pattern._Alpha -= RATE.colorPlusRate / 10000f;
-						saturation._ValueBrightness += RATE.colorPlusRate / 20000f;
+//						pattern._Alpha = 1 - saturation._Saturation;
+			saturation._Saturation += RATE.colorPlusRate / 2000f;
+//						pattern._Alpha -= RATE.colorPlusRate / 10000f;
+			saturation._ValueBrightness += RATE.colorPlusRate / 4000f;
 						
-				} else {
+		} else {
 
-						//////SUCCESS
-						///
-						Instantiate (effectSuccess, transform.position, Quaternion.identity);
-						endPlus ();
-						audio.PlayOneShot (sSuccess);
-						CancelInvoke ("minusColor");
-						CancelInvoke ("plusColor");
-						animation.Play ();
-						//success
-						STATE.mats++;
-						Debug.Log ("mats" + STATE.mats + " " + STATE.matsAll);
-						if (STATE.mats == STATE.matsAll)
-								STATE._STATE = "gSUCCESS";
+			//////SUCCESS
+			///
+			saturation._Saturation = 1;
+			//						pattern._Alpha -= RATE.colorPlusRate / 10000f;
+			saturation._ValueBrightness = 1;
+			Instantiate (effectSuccess, transform.position, Quaternion.identity);
+			endPlus ();
+			audio.PlayOneShot (sSuccess);
+			CancelInvoke ("minusColor");
+			CancelInvoke ("plusColor");
+			animation.Play ("bloom");
+			//success
+			STATE.mats++;
+			Debug.Log ("mats" + STATE.mats + " " + STATE.matsAll);
+			if (STATE.mats == STATE.matsAll)
+				STATE._STATE = "gSUCCESS";
 			
-				}
 		}
+	}
 
-		void endPlus ()
-		{
-				if (audio.isPlaying)
-						audio.Stop ();
+	void endPlus ()
+	{
+		if (audio.isPlaying)
+			audio.Stop ();
 //				if (whiteMat.animation.isPlaying)
 //						whiteMat.animation.Stop ();
-//				if (animation.isPlaying)
-//						animation.Stop ();
+		if (animation.isPlaying)
+			animation.Stop ();
 //				pattern.enabled = false;
-				oClone.SetActive (false);
+//				oClone.SetActive (false);
 //				saturation.enabled = true;
-		}
+	}
 
-		void starPlus ()
-		{
-				if (!audio.isPlaying)
-						audio.Play ();
-				//						if (!whiteMat.animation.isPlaying)
-				//								whiteMat.animation.Play ();
-//				if (!animation.isPlaying)
-//						animation.Play ();
-				oClone.SetActive (true);
+	void starPlus ()
+	{
+		if (!audio.isPlaying)
+			audio.Play ();
+		//						if (!whiteMat.animation.isPlaying)
+		//								whiteMat.animation.Play ();
+		if (!animation.isPlaying)
+			animation.Play ();
+//				oClone.SetActive (true);
 
 //				pattern.enabled = true;
 //				saturation.enabled = false;
 				
-		}
+	}
 
-		void minusColor ()
-		{
-				if (isMinus && saturation._Saturation > 0) {
-						endPlus ();
-						saturation._Saturation -= RATE.colorPlusRate / 10000f;
-						saturation._ValueBrightness -= RATE.colorPlusRate / 20000f;
+	void minusColor ()
+	{
+		if (isMinus && saturation._Saturation > 0) {
+			endPlus ();
+			saturation._Saturation -= RATE.colorPlusRate / 2000f;
+			saturation._ValueBrightness -= RATE.colorPlusRate / 4000f;
 			
 //						whiteMatSprite.color = new Color (1, 1, 1);
-				}
 		}
+	}
 
-		void onColor ()
-		{
-				isPlus = true;
-				isMinus = false;
-		}
+	void onColor ()
+	{
+		isPlus = true;
+		isMinus = false;
+	}
 
-		void offColor ()
-		{
-				isMinus = true;
-				isPlus = false;
-		}
+	void offColor ()
+	{
+		isMinus = true;
+		isPlus = false;
+	}
  
 }
