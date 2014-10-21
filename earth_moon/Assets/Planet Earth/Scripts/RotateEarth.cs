b76a5719544c9231824b2dd05efaf143cecb5b86
acttoz <acttoz@2f1ed029-a5f6-0410-9981-ff4d102dcfa4>
2014-10-21 13:00:50 +0000

    git-svn-id: https://dev.naver.com/svn/black13@163 2f1ed029-a5f6-0410-9981-ff4d102dcfa4

--- earth_moon/Assets/Planet Earth/Scripts/RotateEarth.cs
+++ earth_moon/Assets/Planet Earth/Scripts/RotateEarth.cs
@@ -1,33 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class RotateEarth : MonoBehaviour {
+public class RotateEarth : MonoBehaviour
+{
 
-	private float mouseXAmount, mouseYAmount;
-	// Use this for initialization
-	void Start () {
+		private float mouseXAmount, mouseYAmount;
+		public bool isEarth;
+		// Use this for initialization
+		void Start ()
+		{
 		
-	}
+		}
 	
-	// Update is called once per frame
-	void Update () {
+		// Update is called once per frame
+		void Update ()
+		{
+				int fingerCount = 0;
+				foreach (Touch touch in Input.touches) {
+						if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
+								fingerCount++;
+			
+				}
 		
-		if(Input.GetMouseButton(1)){
-			mouseXAmount = -Input.GetAxis("Mouse X") * 2f;
-			mouseYAmount = Input.GetAxis("Mouse Y") * 2f;
-			transform.Rotate(mouseYAmount,mouseXAmount,0,Space.World);
-			Debug.Log("button");
-			
+		
+				if (fingerCount < 2) {
+						if (Input.GetMouseButton (0)) {
+								mouseXAmount = -Input.GetAxis ("Mouse X") * 3f;
+								if (!isEarth)
+										transform.Rotate (0, mouseXAmount, 0);
+								else
+					transform.Rotate (0, 0, mouseXAmount);
+					
+						}
+				}
+		 
 		}
-		if(Input.GetKey(KeyCode.LeftArrow)){
-			transform.Rotate(0,0,0.1f);
-			Debug.Log("left");
-			
-		}
-		if(Input.GetKey(KeyCode.RightArrow)){
-			transform.Rotate(0,0,-0.1f);
-			Debug.Log("right");
-			
-		}
-	}
 }
---
+++ earth_moon/Assets/camera.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera : MonoBehaviour
+{
+	public int rotateRate;
+		public GameObject cameraPoint;
+		bool moveCamera = false;
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				if (moveCamera) {
+						float step = 5000f * Time.deltaTime;
+			float step2 = rotateRate * Time.deltaTime;
+						transform.position = Vector3.MoveTowards (transform.position, cameraPoint.transform.position, step);
+						transform.rotation = Quaternion.RotateTowards (transform.rotation, cameraPoint.transform.rotation, step2);
+						if (transform.position == cameraPoint.transform.position && transform.rotation == cameraPoint.transform.rotation) {
+				
+								moveCamera = false;
+//								StartCoroutine (cameraWait ());
+				
+						}
+				}
+		}
+
+		void cameraWork ()
+		{
+				moveCamera = true;
+		}
+}
