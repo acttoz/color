7940ceeda7737209736c78b4d5476cba38ed809a
acttoz <acttoz@2f1ed029-a5f6-0410-9981-ff4d102dcfa4>
2014-10-22 07:31:30 +0000

    git-svn-id: https://dev.naver.com/svn/black13@164 2f1ed029-a5f6-0410-9981-ff4d102dcfa4

--- earth_moon/Assets/camera.cs
+++ earth_moon/Assets/camera.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 public class camera : MonoBehaviour
 {
-	public int rotateRate;
+		public int rotateRate;
 		public GameObject cameraPoint;
+		public GameObject earth;
+		public GameObject sun;
 		bool moveCamera = false;
 		// Use this for initialization
 		void Start ()
 		{
+				moveCamera = false;
 	
 		}
 	
@@ -17,7 +20,7 @@ public class camera : MonoBehaviour
 		{
 				if (moveCamera) {
 						float step = 5000f * Time.deltaTime;
-			float step2 = rotateRate * Time.deltaTime;
+						float step2 = rotateRate * Time.deltaTime;
 						transform.position = Vector3.MoveTowards (transform.position, cameraPoint.transform.position, step);
 						transform.rotation = Quaternion.RotateTowards (transform.rotation, cameraPoint.transform.rotation, step2);
 						if (transform.position == cameraPoint.transform.position && transform.rotation == cameraPoint.transform.rotation) {
@@ -27,6 +30,12 @@ public class camera : MonoBehaviour
 				
 						}
 				}
+
+		}
+
+		void parentToEarth ()
+		{
+				transform.parent = earth.transform;
 		}
 
 		void cameraWork ()
